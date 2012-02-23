00eee459ae25e51d98f5187a72ce81a11e108409
Evan Dandrea <evan.dandrea@canonical.com>
2012-02-23 10:57:22 +0000

    Small fixes to the cassandra setup script.

--- test/init_cassandra.sh
+++ test/init_cassandra.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 exec >/var/log/cloud-init.log 2>&1
-echo "deb http://www.apache.org/dist/cassandra debian 10x main" >> /etc/apt/sources.list
+echo "deb http://www.apache.org/dist/cassandra/debian 10x main" >> /etc/apt/sources.list
 gpg --keyserver pgp.mit.edu --recv-keys F758CE318D77295D
 gpg --export --armor F758CE318D77295D | sudo apt-key add -
 gpg --keyserver pgp.mit.edu --recv-keys 2B5C1B00
 gpg --export --armor 2B5C1B00 | sudo apt-key add -
-packages="cassandra"
+packages="cassandra bzr"
 sudo apt-get update
 DEBCONF_FRONTEND=noninteractive sudo apt-get -y install $packages
 bzr branch lp:~ev/oops-repository/whoopsie-daisy /tmp/oops-repository
