e5f571858b8ecdbedcea90e3836e89c2874c2adf
Evan Dandrea <evan.dandrea@canonical.com>
2012-02-28 00:34:32 +0000

    * Fixes to the backend cloud setup:   - Look for the correct init job in create_instance.   - Add a program to drive setting up both Apache and Cassandra and connecting     them.

--- test/init_cassandra.sh
+++ test/init_cassandra.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 exec >/var/log/cloud-init.log 2>&1
 echo "deb http://www.apache.org/dist/cassandra/debian 08x main" >> /etc/apt/sources.list
 gpg --keyserver pgp.mit.edu --recv-keys F758CE318D77295D
@@ -9,9 +10,9 @@ packages="cassandra bzr python-pip"
 sudo apt-get update
 DEBCONF_FRONTEND=noninteractive sudo apt-get -y install $packages
 sed -i 's/rpc_address:.*/rpc_address: 0.0.0.0/g' /etc/cassandra/cassandra.yaml
-sudo /etc/init.d/cassandra restart
 echo "connect localhost/9160; 
 create keyspace crashdb;" | cassandra-cli -B
+sudo /etc/init.d/cassandra restart
 sudo easy_install -U distribute
 sudo pip install pycassa
 bzr branch lp:~ev/oops-repository/whoopsie-daisy /tmp/oops-repository
