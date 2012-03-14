e68b8020a6d34561711daed6355fd773b2f8b0bd
Evan Dandrea <evan.dandrea@canonical.com>
2012-03-14 11:36:11 +0000

    License daisy under the AGPLv3

--- schema.py
+++ schema.py
@@ -1,3 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 
+# Copyright © 2011-2012 Canonical Ltd.
+# Author: Evan Dandrea <evan.dandrea@canonical.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero Public License for more details.
+# 
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from pycassa.system_manager import (
     SystemManager,
     UTF8_TYPE,
