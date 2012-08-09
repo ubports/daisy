64f4dd35471ae913184183656a8e9752eeac3eb4
Evan Dandrea <evan.dandrea@canonical.com>
2012-08-09 18:22:49 +0100

    Use the new SystemIdentifier column and break down to the release level, rather than getting the 90 day total for all releases combined.

--- tools/90day_unique_users.py
+++ tools/90day_unique_users.py
@@ -16,7 +16,8 @@ if not configuration:
 pool = pycassa.ConnectionPool(configuration.cassandra_keyspace,
                               [configuration.cassandra_host], timeout=15)
 
-dayusers_cf = pycassa.ColumnFamily(pool, 'DayUsers')
+dayoops_cf = pycassa.ColumnFamily(pool, 'DayOOPS')
+oops_cf = pycassa.ColumnFamily(pool, 'OOPS')
 
 def _date_range_iterator(start, finish):
     # Iterate all the values including and between the start and finish date
@@ -26,20 +27,34 @@ def _date_range_iterator(start, finish):
         start += datetime.timedelta(days=1)
 
 if __name__ == '__main__':
-    total = set()
     today = datetime.date.today()
     i = _date_range_iterator(today - datetime.timedelta(days=90), today)
+    total = []
+    no_identifier = 0
     for date in i:
         print 'looking up', date
-        start = ''
+        oopses = set()
+        start = pycassa.util.convert_time_to_uuid(0)
         while True:
             try:
-                buf = dayusers_cf.get(date, column_start=start, column_count=1000)
+                buf = dayoops_cf.get(date, column_start=start, column_count=1000)
             except NotFoundException:
                 break
-            buf = buf.keys()
-            total.update(set(buf))
-            start = buf[-1]
+            start = buf.keys()[-1]
+            buf = buf.values()
+            oopses.update(set(buf))
             if len(buf) < 1000:
                 break
-    print len(total)
+        for oops in oopses:
+            try:
+                d = oops_cf.get(oops, columns=['DistroRelease', 'SystemIdentifier'])
+            except NotFoundException:
+                continue
+            if 'DistroRelease' in d and d['DistroRelease'] == 'Ubuntu 12.04':
+                try:
+                    total.append(d['SystemIdentifier'])
+                except KeyError:
+                    no_identifier += 1
+    total = set(total)
+    print 'total', len(total)
+    print 'no identifier', no_identifier
