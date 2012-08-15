5c3b8aea5776a5792a87399193cae244b8767315
Evan Dandrea <evan.dandrea@canonical.com>
2012-08-15 14:19:48 +0100

    Add a program to calculate the number of unique users over a 90 day period.

---
+++ tools/90day_unique_users_for_day.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import sys
+import time
+import pycassa
+from pycassa.cassandra.ttypes import NotFoundException, InvalidRequestException
+import datetime
+import itertools
+
+configuration = None
+try:
+    import local_config as configuration
+except ImportError:
+    pass
+
+if not configuration:
+    import configuration
+
+pool = pycassa.ConnectionPool(configuration.cassandra_keyspace,
+                              [configuration.cassandra_host], timeout=600)
+
+dayusers_cf = pycassa.ColumnFamily(pool, 'DayUsers')
+
+def _date_range_iterator(start, finish):
+    # Iterate all the values including and between the start and finish date
+    # string.
+    while start <= finish:
+        yield start.strftime('%Y%m%d')
+        start += datetime.timedelta(days=1)
+
+# Main
+
+if __name__ == '__main__':
+    today = datetime.date.today()
+    i = _date_range_iterator(today - datetime.timedelta(days=90), today)
+    users = set()
+    for date in i:
+        start = ''
+        while True:
+            try:
+                buf = dayusers_cf.get('Ubuntu 12.04:%s' % date, column_start=start, column_count=1000)
+            except NotFoundException:
+                break
+            buf = buf.keys()
+            start = buf[-1]
+            users.update(buf)
+            if len(buf) < 1000:
+                break
+    print 'total unique users in 90 days:', len(users)
