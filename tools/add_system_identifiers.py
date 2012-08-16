f52303cda7256adf41ca1c1e2d075f8b8be00757
Evan Dandrea <evan.dandrea@canonical.com>
2012-08-16 18:32:53 +0100

    Add a script to add system identifiers to the OOPS CF.

---
+++ tools/add_system_identifiers.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import pycassa
+from pycassa.cassandra.ttypes import NotFoundException, InvalidRequestException
+import datetime
+
+configuration = None
+try:
+    import local_config as configuration
+except ImportError:
+    pass
+
+if not configuration:
+    import configuration
+
+pool = pycassa.ConnectionPool(configuration.cassandra_keyspace,
+                              [configuration.cassandra_host], timeout=15)
+
+useroops_cf = pycassa.ColumnFamily(pool, 'UserOOPS')
+oops_cf = pycassa.ColumnFamily(pool, 'OOPS')
+
+if __name__ == '__main__':
+    for key, d in useroops_cf.get_range():
+        for oops in d.keys():
+            oops_cf.insert(oops, {'SystemIdentifier' : key })
