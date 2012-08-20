9e25e545824debfd0b4f277bbb0d7cf6999394ab
Evan Dandrea <evan.dandrea@canonical.com>
2012-08-20 10:51:31 +0100

    Add a script to show the number of unique users per day of a release.

---
+++ tools/show_unique_users_per_day.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+import time
+import pycassa
+from pycassa.cassandra.ttypes import NotFoundException, InvalidRequestException
+import datetime
+import itertools
+
+configuration = None
+try:
+    import local_config as configuration
+except ImportError:
+    pass
+
+if not configuration:
+    import configuration
+
+pool = pycassa.ConnectionPool(configuration.cassandra_keyspace,
+                              [configuration.cassandra_host], timeout=600)
+
+dayoops_cf = pycassa.ColumnFamily(pool, 'DayOOPS')
+dayusers_cf = pycassa.ColumnFamily(pool, 'DayUsers')
+oops_cf = pycassa.ColumnFamily(pool, 'OOPS')
+
+# Utilities
+
+def _date_range_iterator(start, finish):
+    # Iterate all the values including and between the start and finish date
+    # string.
+    while start <= finish:
+        yield start.strftime('%Y%m%d')
+        start += datetime.timedelta(days=1)
+
+# Main
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        start = datetime.datetime.strptime(sys.argv[1], '%Y%m%d')
+        end = datetime.datetime.strptime(sys.argv[2], '%Y%m%d')
+        i = _date_range_iterator(start, end)
+    else:
+        today = datetime.date.today()
+        i = _date_range_iterator(today - datetime.timedelta(days=90), today)
+    for date in i:
+        print date, dayusers_cf.get_count('Ubuntu 12.04:%s' % date)
