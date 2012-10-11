8e2e458cc68106e786955a0b238bfd0adf463694
Evan Dandrea <evan.dandrea@canonical.com>
2012-10-11 10:49:21 +0100

    Add script to back populate counts by problem type.

---
+++ tools/back_populate_counts_by_problem_type.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+import sys
+import time
+import pycassa
+from pycassa.cassandra.ttypes import NotFoundException, InvalidRequestException
+import datetime
+import itertools
+from collections import defaultdict
+
+configuration = None
+try:
+    import local_config as configuration
+except ImportError:
+    pass
+
+if not configuration:
+    import configuration
+
+creds = {'username': configuration.cassandra_username,
+         'password': configuration.cassandra_password}
+pool = pycassa.ConnectionPool(configuration.cassandra_keyspace,
+                              configuration.cassandra_hosts, timeout=600,
+                              credentials=creds)
+
+dayoops_cf = pycassa.ColumnFamily(pool, 'DayOOPS')
+oops_cf = pycassa.ColumnFamily(pool, 'OOPS')
+counters_cf = pycassa.ColumnFamily(pool, 'Counters')
+
+# Main
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print >>sys.stderr, "Usage: release_name [date]"
+        sys.exit(1)
+    users = set()
+    start = ''
+    date = sys.argv[1]
+    while True:
+        try:
+            buf = dayoops_cf.get(date, column_start=start, column_count=1000)
+        except NotFoundException:
+            break
+        start = buf.keys()[-1]
+        buf = buf.values()
+        users.update(buf)
+        if len(buf) < 1000:
+            break
+    results = defaultdict(int)
+    for uuid in users:
+        try:
+            data = oops_cf.get(str(uuid), columns=['ProblemType', 'DistroRelease'])
+            if not data['DistroRelease'].startswith('Ubuntu '):
+                continue
+            key = '%s:%s' % (data['ProblemType'], data['DistroRelease'])
+        except (NotFoundException, KeyError):
+            # Sometimes we didn't insert the full OOPS. I have no idea why.
+            #print 'could not find', uuid
+            continue
+        results[key] += 1
+    for result in results:
+        k = 'oopses:%s' % result
+        try:
+            v = counters_cf.get(k, columns=[date])
+            print k, date, 'already exists! Skipping.', v
+        except NotFoundException:
+            print 'adding', k, date, results[result]
+            counters_cf.add(k, date, results[result])
+    print results
