9674d2fb658e86e64a02a16fa66b049826c6f12e
Evan Dandrea <evan.dandrea@canonical.com>
2013-06-05 12:36:48 +0100

    mod_wsgi doesn't like lists in the custom headers.

--- daisy/version_middleware.py
+++ daisy/version_middleware.py
@@ -27,8 +27,8 @@ class VersionMiddleware(object):
         def custom_start_response(status, headers, exc_info=None):
             if 'revno' in version_info:
                 rev = version_info['revno']
-                headers.append(['X-Daisy-Revision-Number', rev])
+                headers.append(('X-Daisy-Revision-Number', rev))
             ver = '.'.join(str(component) for component in __version__[0:3])
-            headers.append(['X-Oops-Repository-Version', ver])
+            headers.append(('X-Oops-Repository-Version', ver))
             return start_response(status, headers, exc_info)
         return self.app(environ, custom_start_response)
