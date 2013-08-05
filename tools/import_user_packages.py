a862d3ec40160e625083178200d2464c7b3387f9
Brian Murray <brian@canonical.com>
2013-08-05 09:05:34 -0700

    update teams for whom package subscriptions are cached

--- tools/import_user_packages.py
+++ tools/import_user_packages.py
@@ -19,6 +19,8 @@ if __name__ == '__main__':
     teams = ['ubuntu-x-swat', 'desktop-packages', 'ubuntu-server',
         'foundations-bugs', 'dx-packages', 'edubuntu-bugs', 'kubuntu-bugs',
         'lubuntu-packaging', 'xubuntu-bugs', 'ubuntu-security', 'laney',
-        'kernel-packages']
+        'kernel-packages', 'ubuntu-apps-bugs', 'ubuntu-phonedations-bugs',
+        'ubuntu-sdk-bugs', 'ubuntuone-hackers', 'unity-api-bugs',
+        'unity-ui-bugs']
     for team in teams:
         import_user_binary_packages(team)
