1e8fa379771258db19945d2757d6a640daaee598
Brian Murray <brian@canonical.com>
2013-08-21 11:49:09 -0700

    import_user_packages.py: add ubuntu-webapps-bugs to list of teams to cache

--- tools/import_user_packages.py
+++ tools/import_user_packages.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         'foundations-bugs', 'dx-packages', 'edubuntu-bugs', 'kubuntu-bugs',
         'lubuntu-packaging', 'xubuntu-bugs', 'ubuntu-security-bugs', 'laney',
         'kernel-packages', 'ubuntu-apps-bugs', 'ubuntu-phonedations-bugs',
-        'ubuntu-sdk-bugs', 'ubuntuone-hackers', 'unity-api-bugs',
-        'unity-ui-bugs']
+        'ubuntu-sdk-bugs', 'ubuntu-webapps-bugs', 'ubuntuone-hackers',
+        'unity-api-bugs', 'unity-ui-bugs']
     for team in teams:
         import_user_binary_packages(team)
