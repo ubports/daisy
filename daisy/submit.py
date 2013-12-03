ea095c7464d1380cf31087372885888259e9fbea
Brian Murray <brian@canonical.com>
2013-12-03 10:19:11 -0800

    submit.py: update a counter for rls:src_pkg:src_version to better track the crashiness of a particular package version for regressions

--- daisy/submit.py
+++ daisy/submit.py
@@ -44,6 +44,9 @@ metrics = get_metrics('daisy.%s' % socket.gethostname())
 def update_release_pkg_counter(counters_fam, release, src_package, date):
     counters_fam.insert('%s:%s' % (release, src_package), {date: 1})
 
+def update_release_pkg_version_counter(counters_fam, release, src_package, src_version, date):
+    counters_fam.insert('%s:%s:%s' % (release, src_package, src_version), {date: 1})
+
 def create_report_from_bson(data):
     report = apport.Report()
     for key in data:
@@ -128,6 +131,10 @@ def submit(_pool, environ, system_token):
 
     if not third_party and problem_type == 'Crash':
         update_release_pkg_counter(counters_fam, release, src_package, day_key)
+        if src_version == '':
+            metrics.meter('missing.missing_srcpackage_version')
+        else:
+            update_release_pkg_version_counter(counters_fam, release, src_package, src_version, day_key)
 
     try_to_repair_sas(data)
     oopses.insert_dict(oops_config, oops_id, data, system_token, fields)
