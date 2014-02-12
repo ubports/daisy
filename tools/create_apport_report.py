aacb420c8f1966a0b8585269d24335db19cc966d
Brian Murray <brian@canonical.com>
2014-02-11 18:03:52 -0800

    add in a tool to back populate the rls:srcpkg:vers counter and update create_apport_report.py to add coredumps to reports

--- tools/create_apport_report.py
+++ tools/create_apport_report.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
-import sys
+import os
 import pycassa
+import sys
+
 from apport import report
 from daisy import config
+from subprocess import Popen, PIPE
 
 creds = {'username': config.cassandra_username,
          'password': config.cassandra_password}
@@ -20,12 +23,22 @@ if __name__ == '__main__':
         sys.exit(1)
     core = None
     if len(sys.argv) == 4:
-        core = sys.argv[3]
+        core_file = sys.argv[3]
     oops = oops_cf.get(sys.argv[1])
     r = report.Report()
     for k in oops:
         r[k] = oops[k]
-    if core:
-        r['CoreDump'] = (core,)
+    if core_file:
+        with open(core_file.replace('core', 'coredump'), 'wb') as fp:
+        #r['CoreDump'] = (core,)
+            # 2014-02-11 the coredumps fo0bar gave me were already base64
+            # decoded, if they aren't in the future then you'll need this
+            #p1 = Popen(['base64', '-d', core_file], stdout=PIPE)
+            # Set stderr to PIPE so we get output in the result tuple.
+            #p2 = Popen(['zcat'], stdin=p1.stdout, stdout=fp, stderr=PIPE)
+            p2 = Popen(['zcat', core_file], stdout=fp, stderr=PIPE)
+            ret = p2.communicate()
+        r['CoreDump'] = (core_file.replace('core', 'coredump'),)
     fp = open(sys.argv[2], 'wb')
     r.write(fp)
+    os.remove(core_file.replace('core', 'coredump'))
