d15c8e1cb5c3d0a797b3194ff1aa62942928a2ce
Brian Murray <brian@canonical.com>
2014-04-30 09:39:21 -0700

    tools/remove_unnecessary_data.py: remove data from EoL oopses and always remove Stacktrace and ThreadStacktrace

--- tools/remove_unnecessary_data.py
+++ tools/remove_unnecessary_data.py
@@ -5,7 +5,7 @@ import pycassa
 import sys
 
 from daisy import config
-from pycassa.cassandra.ttypes import NotFoundException
+from pycassa.cassandra.ttypes import NotFoundException, InvalidRequestException
 
 creds = {'username': config.cassandra_username,
          'password': config.cassandra_password}
@@ -17,10 +17,11 @@ oops_cf = pycassa.ColumnFamily(pool, 'OOPS')
 indexes_cf = pycassa.ColumnFamily(pool, 'Indexes')
 awaiting_retrace_cf = pycassa.ColumnFamily(pool, 'AwaitingRetrace')
 
+never_needed_columns = ['Stacktrace', 'ThreadStacktrace']
 unneeded_columns = ['Disassembly', 'ProcMaps', 'ProcStatus',
-                    'Registers', 'StacktraceTop', 'Stacktrace',
-                    'ThreadStacktrace']
+                    'Registers', 'StacktraceTop']
 counts = 0
+ire_count = 0
 
 wait_amount = 30000000
 wait = wait_amount
@@ -37,23 +38,44 @@ def print_totals(force=False):
         print
         sys.stdout.flush()
 
-for oops_id, data in oops_cf.get_range(columns=['StacktraceAddressSignature'],
-                                            buffer_size=2*1024):
-    signature = data['StacktraceAddressSignature']
+for oops in oops_cf.get_range(finish='f852276e-8456-11e3-913e-e4115b0f8a4a',
+        columns=['StacktraceAddressSignature','DistroRelease'],
+        buffer_size=2*1024):
+    eol = False
+    oops_id = oops[0]
+    data = oops[1]
+    signature = data.get('StacktraceAddressSignature', '')
+    release = data.get('DistroRelease', '')
     print_totals()
     counts += 1
+    # remove Stacktrace and ThreadStacktrace from everything
+    oops_cf.remove(oops_id, columns=never_needed_columns)
+    # for EoL releases remove the columns
+    if release in ['Ubuntu 13.04']:
+        eol = True
+        print('Removing data for EoL %s' % oops_id)
+        oops_cf.remove(oops_id, columns=unneeded_columns)
     if not signature:
         print("%s has no signature" % oops_id)
         continue
+    # if it failed it can't be awaiting retracing
     if signature.startswith('failed:'):
         print("%s failed to retrace" % oops_id)
         continue
     try:
         awaiting = awaiting_retrace_cf.get(signature)
         print("%s is waiting to retrace" % oops_id)
+        # we don't want to retrace EoL releases
+        if eol:
+            print("Removing retrace of %s" % oops_id)
+            awaiting_retrace_cf.remove(signature)
         continue
     except NotFoundException:
         pass
+    except InvalidRequestException:
+        ire_count +=1
+        print('Long signature count: %s' % ire_count)
+        continue
     try:
         idx = 'retracing'
         crash_signature = indexes_cf.get(idx, [signature])
@@ -62,7 +84,7 @@ for oops_id, data in oops_cf.get_range(columns=['StacktraceAddressSignature'],
     except NotFoundException:
         pass
     print('Removing data for %s' % oops_id)
-    #if counts >= 5:
+    #if counts >= 10:
     #    break
     oops_cf.remove(oops_id, columns=unneeded_columns)
 print_totals(force=True)
