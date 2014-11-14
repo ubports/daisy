6d49111070fafafd878c1b5445cea1b3b499b346
Brian Murray <brian@canonical.com>
2014-11-13 16:36:26 -0800

    tools/oops-missing-package-information.py: switch to using oops reported within the past week instead of just ones reported today

--- tools/oops-missing-package-information.py
+++ tools/oops-missing-package-information.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import pycassa
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from daisy import config
 
@@ -11,10 +11,10 @@ pool = pycassa.ConnectionPool(config.cassandra_keyspace,
                               config.cassandra_hosts, timeout=10,
                               credentials=creds)
 oops_cf = pycassa.ColumnFamily(pool, 'OOPS')
-today = datetime.today()
+old_date = datetime.today() - timedelta(days=7)
 count = 0
 for oops, oops_data in oops_cf.get_range(columns=['Date','Package'],
-        row_count=10000):
+        row_count=1000):
     if count >= 10:
         sys.exit(0)
     date_str = oops_data.get('Date', '')
@@ -22,7 +22,7 @@ for oops, oops_data in oops_cf.get_range(columns=['Date','Package'],
         date = datetime.strptime(date_str, '%a %b %d %H:%M:%S %Y')
     except ValueError:
         continue
-    if date.date() != today.date():
+    if date.date() >= old_date.date():
         continue
     pkg = oops_data.get('Package', '')
     if pkg:
