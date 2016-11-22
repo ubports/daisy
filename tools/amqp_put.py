f82041c7e3bb2c328f49487f7885b2cb8f8cd5fa
Brian Murray <brian@canonical.com>
2016-11-22 12:28:08 -0800

    tools/amqp_put.py: improve handling of arch being empty or NotFound

--- tools/amqp_put.py
+++ tools/amqp_put.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
-import sys
-import os
-import pycassa
-from pycassa.cassandra.ttypes import NotFoundException
-import amqplib.client_0_8 as amqp
-import atexit
-from daisy import config
 
-# requeue core files that are in swift or if they do not exist in the crash
+# requeue a core file that is in swift or if they do not exist in the crash
 # database remove the core file from swift, a core file might be added
 # to the queue an additional time but the retracer will handle that.
 
-# TODO: could just have this code list the queue from swift check to see if
-# the cores are older than 7 days and only requeue those crashes.
+import amqplib.client_0_8 as amqp
+import atexit
+import os
+import pycassa
+import sys
+
+from pycassa.cassandra.ttypes import NotFoundException
+from daisy import config
+
 if len(sys.argv) < 2:
     print >>sys.stderr, 'usage: %s <uuid>'
     sys.exit(1)
@@ -24,18 +24,22 @@ creds = {'username': config.cassandra_username,
 pool = pycassa.ConnectionPool(config.cassandra_keyspace,
                               config.cassandra_hosts, credentials=creds)
 oops_fam = pycassa.ColumnFamily(pool, 'OOPS')
+
+cs = getattr(config, 'core_storage', '')
+if not cs:
+    log('core_storage not set.')
+    sys.exit(1)
+
 arch = ''
 try:
     arch = oops_fam.get(uuid, columns=['Architecture'])['Architecture']
 except NotFoundException:
     print >>sys.stderr, 'could not find architecture for %s' % uuid
-    # if the Architecture can not be found the OOPS doesn't exist in the
-    # database so remove the core file from the swift
+    pass
+# if the Architecture can not be found the OOPS doesn't exist in the
+# database so remove the core file from the swift
+if arch == '':
     import swiftclient
-    cs = getattr(config, 'core_storage', '')
-    if not cs:
-        log('core_storage not set.')
-        sys.exit(1)
     provider_data = cs['swift']
     opts = {'tenant_name': provider_data['os_tenant_name'],
             'region_name': provider_data['os_region_name']}
@@ -49,8 +53,8 @@ except NotFoundException:
     _cached_swift.delete_object(bucket, uuid)
     print >>sys.stderr, 'removed %s from swift' % uuid
     sys.exit(1)
-queue = 'retrace_%s' % arch
 
+queue = 'retrace_%s' % arch
 connection = amqp.Connection(host=config.amqp_host)
 channel = connection.channel()
 atexit.register(connection.close)
