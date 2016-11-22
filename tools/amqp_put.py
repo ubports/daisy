70d004e6a4152dd23096841affe0ccb3832383f0
Brian Murray <brian@canonical.com>
2016-11-21 16:18:22 -0800

    fix requeueing of core files and delete core files that aren't in the DB

--- tools/amqp_put.py
+++ tools/amqp_put.py
@@ -7,6 +7,12 @@ import amqplib.client_0_8 as amqp
 import atexit
 from daisy import config
 
+# requeue core files that are in swift or if they do not exist in the crash
+# database remove the core file from swift, a core file might be added
+# to the queue an additional time but the retracer will handle that.
+
+# TODO: could just have this code list the queue from swift check to see if
+# the cores are older than 7 days and only requeue those crashes.
 if len(sys.argv) < 2:
     print >>sys.stderr, 'usage: %s <uuid>'
     sys.exit(1)
@@ -23,6 +29,25 @@ try:
     arch = oops_fam.get(uuid, columns=['Architecture'])['Architecture']
 except NotFoundException:
     print >>sys.stderr, 'could not find architecture for %s' % uuid
+    # if the Architecture can not be found the OOPS doesn't exist in the
+    # database so remove the core file from the swift
+    import swiftclient
+    cs = getattr(config, 'core_storage', '')
+    if not cs:
+        log('core_storage not set.')
+        sys.exit(1)
+    provider_data = cs['swift']
+    opts = {'tenant_name': provider_data['os_tenant_name'],
+            'region_name': provider_data['os_region_name']}
+    _cached_swift = swiftclient.client.Connection(
+        provider_data['os_auth_url'],
+        provider_data['os_username'],
+        provider_data['os_password'], os_options=opts,
+        auth_version='2.0')
+    bucket = provider_data['bucket']
+    _cached_swift.http_conn = None
+    _cached_swift.delete_object(bucket, uuid)
+    print >>sys.stderr, 'removed %s from swift' % uuid
     sys.exit(1)
 queue = 'retrace_%s' % arch
 
@@ -31,7 +56,8 @@ channel = connection.channel()
 atexit.register(connection.close)
 atexit.register(channel.close)
 channel.queue_declare(queue=queue, durable=True, auto_delete=False)
-body = amqp.Message(path)
+# msg:provider
+body = amqp.Message('%s:swift' % path)
 # Persistent
 body.properties['delivery_mode'] = 2
 channel.basic_publish(body, exchange='', routing_key=queue)
