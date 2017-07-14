f49ca6fcebe46e1167cd3f2fb831409fc8446d39
Brian Murray <brian@canonical.com>
2017-07-14 12:12:23 -0700

    daisy/utils.py: add in yakkety to the list of End of Life releases

--- daisy/utils.py
+++ daisy/utils.py
@@ -26,7 +26,8 @@ EOL_RELEASES = {'Ubuntu 10.04': 'lucid',
                 'Ubuntu RTM 14.09': 'vivid',
                 'Ubuntu 14.10': 'utopic',
                 'Ubuntu 15.04': 'vivid',
-                'Ubuntu 15.10': 'wily'}
+                'Ubuntu 15.10': 'wily',
+                'Ubuntu 16.10': 'yakkety'}
 
 def get_fields_for_bucket_counters(problem_type, release, package, version,
         pkg_arch, rootfs_build, channel, device_name, device_image):
