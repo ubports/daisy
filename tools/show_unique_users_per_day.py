c0e1a67690c9cd427327832cebf824cf8fb32c50
Brian Murray <brian@canonical.com>
2017-10-27 17:19:28 -0700

    tools/show_unique_users_per_day.py: be a lot smarter about how releases are determined.

--- tools/show_unique_users_per_day.py
+++ tools/show_unique_users_per_day.py
@@ -31,18 +31,16 @@ if __name__ == '__main__':
     else:
         today = datetime.date.today()
         i = _date_range_iterator(today - datetime.timedelta(days=10), today)
-    print "Date, 12.04, 14.04, 14.10, 15.04, 15.10, 16.04, 16.10, 17.04, Total"
+    releases = ['12.04', '14.04', '14.10', '15.04', '15.10', '16.04', '16.10',
+                '17.04', '17.10']
+    header = "Date, "
+    for release in releases:
+        header += '%s, ' % release
+    header += 'Total'
+    print header
     for date in i:
-        precise = dayusers_cf.get_count('Ubuntu 12.04:%s' % date)
-        trusty = dayusers_cf.get_count('Ubuntu 14.04:%s' % date)
-        utopic = dayusers_cf.get_count('Ubuntu 14.10:%s' % date)
-        vivid = dayusers_cf.get_count('Ubuntu 15.04:%s' % date)
-        wily = dayusers_cf.get_count('Ubuntu 15.10:%s' % date)
-        xenial = dayusers_cf.get_count('Ubuntu 16.04:%s' % date)
-        yakkety = dayusers_cf.get_count('Ubuntu 16.10:%s' % date)
-        zesty = dayusers_cf.get_count('Ubuntu 17.04:%s' % date)
-        total = (precise + trusty + utopic + vivid + wily + xenial +
-                 yakkety + zesty)
-        print("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % \
-              (date, precise, trusty, utopic, vivid, wily,
-               xenial, yakkety, zesty, total))
+        counts = []
+        for release in releases:
+            count = dayusers_cf.get_count('Ubuntu %s:%s' % (release, date))
+            counts.append(str(count))
+        print('%s, %s' % (date, ', '.join(counts)))
